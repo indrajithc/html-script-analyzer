99e815baa67ed5f204e1e17c486b7b0270df24e3
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-11-26 16:27:26 +0530

    refactor: script logic

--- src/handlers/contentHandler.js
+++ src/handlers/contentHandler.js
@@ -5,6 +5,51 @@ import { JSDOM } from "jsdom";
 
 const { contentDirectory, contentConfigDirectory } = config;
 
+const processFilename = "index.html";
+const scriptMetadataFilename = "script-metadata.json";
+
+const getUniqueId = () => {
+  return Math.random().toString(36).substr(2, 9);
+};
+
+const doFirstTimeActivity = (sourceFilePath, destinationDirectory) => {
+  const filePath = path.join(destinationDirectory, processFilename);
+
+  const content = fs.readFileSync(sourceFilePath, "utf-8");
+
+  const dom = new JSDOM(content);
+  const { document } = dom.window;
+
+  // list all the script tags
+  const scriptTags = document.querySelectorAll("script");
+
+  const scriptList = [];
+
+  scriptTags.forEach((scriptTag) => {
+    const src = scriptTag.getAttribute("src");
+    if (!scriptTag.id) {
+      scriptTag.id = getUniqueId();
+    }
+    scriptList.push({
+      id: scriptTag.id,
+      src: src,
+      content: scriptTag.innerHTML,
+    });
+  });
+
+  try {
+    fs.writeFileSync(
+      path.join(destinationDirectory, scriptMetadataFilename),
+      JSON.stringify(scriptList)
+    );
+  } catch (error) {
+    console.error("Failed to save script metadata", error);
+  }
+
+  fs.writeFileSync(filePath, dom.serialize());
+  return filePath;
+};
+
 export const handleContentRequest = async (req) => {
   const { pathname, searchParams } = new URL(req.url);
   const referer = req.headers.get("Referer");
@@ -16,8 +61,20 @@ export const handleContentRequest = async (req) => {
     return new Response("File not found", { status: 404 });
   }
 
+  const processingDirectory = path.join(contentConfigDirectory, fileName);
+
+  if (!fs.existsSync(processingDirectory)) {
+    fs.mkdirSync(processingDirectory);
+  }
+
+  const processingFilePath = path.join(processingDirectory, processFilename);
+
+  if (!fs.existsSync(processingFilePath)) {
+    await doFirstTimeActivity(filePath, processingDirectory);
+  }
+
   if (referer) {
-    const content = fs.readFileSync(filePath, "utf-8");
+    const content = fs.readFileSync(processingFilePath, "utf-8");
 
     const dom = new JSDOM(content);
     const { document } = dom.window;
@@ -27,6 +84,17 @@ export const handleContentRequest = async (req) => {
     return new Response(html, {
       headers: { "Content-Type": "text/html" },
     });
+  }
+
+  const scriptMetadataPath = path.join(
+    processingDirectory,
+    scriptMetadataFilename
+  );
+  let scriptMetadata = null;
+  try {
+    scriptMetadata = JSON.parse(fs.readFileSync(scriptMetadataPath, "utf-8"));
+  } catch (error) {
+    console.error("Failed to read script metadata", error);
   }
 
   return new Response(
@@ -75,17 +143,58 @@ export const handleContentRequest = async (req) => {
           iframe.style.display = 'block'; // Show iframe
         }
       </script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+
     </head>
     <body>
       <div class="container-fluid py-2 h-100">
         <div class="row h-100">
-          <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+          <div class="col-12 col-md-6 col-lg-5">
             <div class="card h-100">
               <div class="card-header">
                 <h3>${fileName}</h3>
               </div>
-              <div class="card-body">
-                 <button onclick="refreshIframe()" class="btn btn-primary w-100 btn-lg">Refresh Site</button>
+              <div class="card-body"> 
+              <button onclick="refreshIframe()" class="btn btn-primary w-100 btn-lg">Refresh Site</button>
+              
+              ${
+                Array.isArray(scriptMetadata)
+                  ? `
+                <div class="overflow-y-auto overflow-x-hidden" style="height: calc(100vh - 150px);">
+                  <div class="accordion" id="accordionExample">
+              ${scriptMetadata
+                .map(
+                  (script, index) => `
+                  <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading${index}">
+                      <div class="accordion-button d-flex flex-column " type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
+                        <div>${script.id}</div> 
+                        <small class="text-wrap text-muted">${
+                          script.src || "Inline script"
+                        }</small>
+                      </div>
+                    </h2> 
+                    ${
+                      script.content
+                        ? `
+                    <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#accordionExample">
+                      <div class="accordion-body">
+                      <pre><code class="language-js" id="code_${index}"></code></pre>
+                      </div>
+                    </div>
+                    `
+                        : ""
+                    }
+                  </div>`
+                )
+                .join("")}
+              </div>
+              </div>
+              
+              `
+                  : '<p class="text-muted text-center mt-4">No scripts found</p>'
+              }
+              
               </div>
             </div>
           </div>
@@ -105,6 +214,27 @@ export const handleContentRequest = async (req) => {
           </div>
         </div>
       </div>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+<script>
+${
+  Array.isArray(scriptMetadata)
+    ? scriptMetadata
+        .map((script, index) =>
+          script?.content
+            ? `
+document.getElementById('code_${index}').innerText = \`${script.content.replace(
+                /`/g,
+                "\\`"
+              )}\`;
+hljs.highlightElement(document.getElementById('code_${index}'));
+`
+            : ""
+        )
+        .join("\n")
+    : ""
+}
+</script>  
     </body>
   </html>
   `,
